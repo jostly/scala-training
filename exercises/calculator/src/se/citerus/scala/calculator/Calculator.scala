404910578e1c73f4f2faae1f0114a025ceda0fd3
Johan Östling <johan.f.ostling@gmail.com>
2016-01-27 17:15:18 +0100

    Refactor Calculator to allow testing of input/output. Add tests to cover more functionality

--- exercises/calculator/src/se/citerus/scala/calculator/Calculator.scala
+++ exercises/calculator/src/se/citerus/scala/calculator/Calculator.scala
@@ -7,8 +7,6 @@ class Calculator(accumulatedGross: BigDecimal,
                  limit: BigDecimal = BigDecimal(100),
                  discountPercentage: BigDecimal = BigDecimal(10)) {
 
-  require(accumulatedGross >= 0, "accumulated gross must be 0 or greater")
-
   def amountToPay(purchaseAmount: BigDecimal): BigDecimal =
     if (accumulatedGross >= limit) {
       discountOf(purchaseAmount)
@@ -26,33 +24,37 @@ class Calculator(accumulatedGross: BigDecimal,
 
 object Calculator {
 
-  def calculate(accumulatedGross: BigDecimal, purchaseAmount: BigDecimal): (BigDecimal, BigDecimal) = {
+  private def calculate(accumulatedGross: BigDecimal, purchaseAmount: BigDecimal): (BigDecimal, BigDecimal) = {
     val amountToPay = new Calculator(accumulatedGross = accumulatedGross).amountToPay(purchaseAmount)
 
     (accumulatedGross + purchaseAmount, amountToPay)
   }
 
+  def run(prompt: String => String, display: String => Unit): Unit = {
+    def readValue(p: String): BigDecimal = {
+      val v = BigDecimal(prompt(p))
+      if (v < 0) throw new IllegalArgumentException("Amount must be 0 or greater")
+      v
+    }
 
-  def main(args: Array[String]): Unit = {
-
-    val input = readInput
+    val input = Try(readValue("Accumulated purchase value: "))
+      .flatMap(av => Try((av, readValue("Purchase amount: "))))
 
     input.map(i => calculate(i._1, i._2)) match {
       case Success((newAccumulatedGross, amountToPay)) =>
-        println(s"Amount to pay: $amountToPay")
-        println(s"New accumulated gross: $newAccumulatedGross")
+        display(s"Amount to pay: $amountToPay")
+        display(s"New accumulated purchase value: $newAccumulatedGross")
       case Failure(e: NumberFormatException) =>
-        println("Input a valid decimal number")
+        display("Please input a decimal number")
+      case Failure(e: IllegalArgumentException) =>
+        display(e.getMessage)
       case Failure(e) =>
-        println("Illegal input: " + e.getMessage)
+        display(e.toString)
     }
-
   }
 
-  def readInput: Try[(BigDecimal, BigDecimal)] = {
-    Try((
-      BigDecimal(readLine("Accumulated gross purchases: ")),
-      BigDecimal(readLine("Purchase amount: "))
-      ))
+  def main(args: Array[String]): Unit = {
+    run(s => readLine(s), s => println(s))
   }
+
 }
--- exercises/calculator/test/se/citerus/scala/calculator/CalculatorTest.scala
+++ exercises/calculator/test/se/citerus/scala/calculator/CalculatorTest.scala
@@ -2,6 +2,8 @@ package se.citerus.scala.calculator
 
 import org.scalatest.FunSuite
 
+import scala.collection.mutable
+
 class CalculatorTest extends FunSuite {
 
   test("no discount below limit") {
@@ -36,4 +38,59 @@ class CalculatorTest extends FunSuite {
     assert(amountToPay === BigDecimal(95))
   }
 
+  test("run calculator") {
+    val io = new IoStub(
+      "Accumulated purchase value: " -> "50",
+      "Purchase amount: " -> "100"
+    )
+
+    Calculator.run(io.prompt, io.display)
+
+    assert(io.displays === List(
+      "Amount to pay: 95",
+      "New accumulated purchase value: 150"
+    ))
+  }
+
+  test("inputting something other than a decimal number for accumulated value gives error") {
+    val io = new IoStub("Accumulated purchase value: " -> "arghdsd")
+
+    Calculator.run(io.prompt, io.display)
+
+    assert(io.displays === List("Please input a decimal number"))
+  }
+
+  test("inputting something other than a decimal number for purchase value gives error") {
+    val io = new IoStub(
+      "Accumulated purchase value: " -> "0",
+      "Purchase amount: " -> "hnjkd"
+    )
+
+    Calculator.run(io.prompt, io.display)
+
+    assert(io.displays === List("Please input a decimal number"))
+  }
+
+  test("inputting a negative number for accumulated value gives error") {
+    val io = new IoStub("Accumulated purchase value: " -> "-1")
+
+    Calculator.run(io.prompt, io.display)
+
+    assert(io.displays === List("Amount must be 0 or greater"))
+  }
+}
+
+class IoStub(expectedPrompts: (String, String)*) {
+  private val _prompts = mutable.Map(expectedPrompts: _*)
+  private val _displays = mutable.ListBuffer.empty[String]
+
+  def prompt(p: String): String = {
+    _prompts.remove(p).getOrElse(throw new RuntimeException(s"Unexpected prompt $p"))
+  }
+
+  def display(s: String): Unit = {
+    _displays += s
+  }
+
+  def displays: List[String] = _displays.toList
 }
